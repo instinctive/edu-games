8cec9efa9673fd79598cb1439d343a9bcd604917
instinctive <177694+instinctive@users.noreply.github.com>
2026-01-13 10:50:24 -0800

    Noodle around with randomGen and trees

--- src/mct/MCT.hs
+++ src/mct/MCT.hs
@@ -3,13 +3,23 @@ module MCT where
 import Control.Lens
 import Control.Monad.Random.Class
 import Data.Array.Unboxed
+import System.Random ( split )
 
 import Game
 
 data Tree a = Tree a [Tree a] deriving (Show, Functor)
 
+mkTree :: (a -> [a]) -> a -> Tree a
+mkTree f a = Tree a (mkTree f <$> f a)
+
+splitGens g = let (a,b) = split g in a : splitGens b
+
+annoRandom g (Tree a tt) =
+    Tree (g',a) (zipWith annoRandom gg tt)
+  where
+    g':gg = splitGens g
+
 data MCT p = MCT
-    -- _mctId    :: Int  -- for psqueue?
     { _mctWins  :: UArray p Int
     , _mctPlays :: Int
     } deriving Show
@@ -26,8 +36,6 @@ pickOne l = getRandomR (0, length l - 1) <&> (l!!)
 rollout g
     | gameStatus g == Play = pickOne (nextGames g) >>= rollout
     | otherwise = pure g
-
-mkTree g = Tree g (mkTree <$> nextGames g)
 
 rolloutTree (Tree g []) = pure g
 rolloutTree (Tree _ tt) = pickOne tt >>= rolloutTree
