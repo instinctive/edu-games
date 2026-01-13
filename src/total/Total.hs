03b6e83fff47d16036e6e727b6a296acea211cf0
instinctive <177694+instinctive@users.noreply.github.com>
2026-01-13 15:13:16 -0800

    Remove TotalArray newtype

--- src/total/Total.hs
+++ src/total/Total.hs
@@ -2,6 +2,8 @@ module Total where
 
 import Data.Array.Base
 import Data.Array.IArray
+import Data.Array.IArray qualified as I
+import Data.Array.Unboxed qualified as U
 import GHC.Ix ( unsafeIndex )
 
 #define ENUM(a) Eq a, Ord a, Enum a, Bounded a, Ix a, Show a
@@ -10,27 +12,28 @@ type TOTAL a i e = (IArray a e, ENUM(i))
 
 type Lens' s a = forall f. Functor f => (a -> f a) -> s -> f s
 
-newtype TotalArray arr i e = TotalArray (arr i e)
+type Array = I.Array
+type UArray = U.UArray
 
-totalAt :: TOTAL a i e => i -> Lens' (TotalArray a i e) e
-totalAt i f (TotalArray a) =
+totalAt :: TOTAL a i e => i -> Lens' (a i e) e
+totalAt i f a =
     let n = unsafeIndex (minBound,maxBound) i in
-    f (unsafeAt a n) <&> \e -> TotalArray (unsafeReplace a [(n,e)])
+    f (unsafeAt a n) <&> \e -> unsafeReplace a [(n,e)]
 
-totalArray :: TOTAL a i e => (i -> e) -> TotalArray a i e
-totalArray f = TotalArray $ listArray (minBound, maxBound) $ f <$> [minBound..maxBound]
+totalArray :: TOTAL a i e => (i -> e) -> a i e
+totalArray f = listArray (minBound, maxBound) $ f <$> [minBound..maxBound]
 
-(!) :: TOTAL a i e => TotalArray a i e -> i -> e
-(!) (TotalArray a) i = unsafeAt a (unsafeIndex (minBound,maxBound) i)
+(!) :: TOTAL a i e => a i e -> i -> e
+(!) a i = unsafeAt a (unsafeIndex (minBound,maxBound) i)
 
-(//) :: TOTAL a i e => TotalArray a i e -> [(i,e)] -> TotalArray a i e
-(//) (TotalArray a) l = TotalArray $ unsafeReplace a $ first (unsafeIndex (minBound,maxBound)) <$> l
+(//) :: TOTAL a i e => a i e -> [(i,e)] -> a i e
+(//) a l = unsafeReplace a $ first (unsafeIndex (minBound,maxBound)) <$> l
 
-indices :: TOTAL a i e => TotalArray a i e -> [i]
+indices :: TOTAL a i e => a i e -> [i]
 indices _ = [minBound..maxBound]
 
-elems :: TOTAL a i e => TotalArray a i e -> [e]
-elems (TotalArray a) = Data.Array.IArray.elems a
+elems :: TOTAL a i e => a i e -> [e]
+elems a = I.elems a
 
-assocs :: TOTAL a i e => TotalArray a i e -> [(i,e)]
-assocs (TotalArray a) = zip [minBound..maxBound] (Data.Array.IArray.elems a)
+assocs :: TOTAL a i e => a i e -> [(i,e)]
+assocs a = zip [minBound..maxBound] (I.elems a)
