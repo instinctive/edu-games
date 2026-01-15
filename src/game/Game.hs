7dce93602994792e7d653c7fcca6583c5a6cb7ed
instinctive <177694+instinctive@users.noreply.github.com>
2026-01-15 08:23:58 -0800

    Cleanup PlayerId

--- src/game/Game.hs
+++ src/game/Game.hs
@@ -1,30 +1,25 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DeriveAnyClass #-}
 
 module Game where
 
 import Data.Kind ( Type )
 
-#define ENUM Eq,Ord,Enum,Bounded,Ix,Show
-data P1 = P1a                         deriving (ENUM)
-data P2 = P2a | P2b                   deriving (ENUM)
-data P3 = P3a | P3b | P3c             deriving (ENUM)
-data P4 = P4a | P4b | P4c | P4d       deriving (ENUM)
-data P5 = P5a | P5b | P5c | P5d | P5e deriving (ENUM)
-#undef ENUM
-
-class (Eq a, Bounded a, Enum a, Ix a, Show a) => PlayerId a where
+class (Eq a, Ord a, Bounded a, Enum a, Ix a, Show a) => PlayerId a where
     numPlayers :: Int
-    numPlayers = fromEnum (maxBound @a) - fromEnum (minBound @a) + 1
     succPlayer :: a -> a
-    succPlayer p = if p == maxBound then minBound else succ p
     predPlayer :: a -> a
+    numPlayers = fromEnum (maxBound @a) - fromEnum (minBound @a) + 1
+    succPlayer p = if p == maxBound then minBound else succ p
     predPlayer p = if p == minBound then maxBound else pred p
 
-instance PlayerId P1
-instance PlayerId P2
-instance PlayerId P3
-instance PlayerId P4
-instance PlayerId P5
+#define PLAYERID Eq,Ord,Bounded,Enum,Ix,Show,PlayerId
+data P1 = P1a                         deriving (PLAYERID)
+data P2 = P2a | P2b                   deriving (PLAYERID)
+data P3 = P3a | P3b | P3c             deriving (PLAYERID)
+data P4 = P4a | P4b | P4c | P4d       deriving (PLAYERID)
+data P5 = P5a | P5b | P5c | P5d | P5e deriving (PLAYERID)
+#undef PLAYERID
 
 data Status p = Play | Draw | Win p deriving (Eq,Ord,Show)
 
