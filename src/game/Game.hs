bd457b084a61432b18007c34fbab6b5b5e8f6fde
Charles Martin <177694+instinctive@users.noreply.github.com>
2026-01-16 14:23:45 -0800

    Refactor complete

--- src/game/Game.hs
+++ src/game/Game.hs
@@ -9,19 +9,25 @@ import Data.List.NonEmpty ( NonEmpty )
 -- | Player class.
 class (Eq a, Ord a, Bounded a, Enum a, Ix a, Show a) => PlayerId a where
     numPlayers :: Int    -- Number of players
+    drawValue  :: Double -- Value of a draw
     succPlayer :: a -> a -- Next player, will wrap around.
     predPlayer :: a -> a -- Previous player, will wrap around.
-    numPlayers = fromEnum (maxBound @a) - fromEnum (minBound @a) + 1
     succPlayer p = if p == maxBound then minBound else succ p
     predPlayer p = if p == minBound then maxBound else pred p
 
-#define PLAYERID Eq,Ord,Bounded,Enum,Ix,Show,PlayerId
-data P1 = P1a                         deriving (PLAYERID) -- ^ 1 Player
-data P2 = P2a | P2b                   deriving (PLAYERID) -- ^ 2 Players
-data P3 = P3a | P3b | P3c             deriving (PLAYERID) -- ^ 3 Players
-data P4 = P4a | P4b | P4c | P4d       deriving (PLAYERID) -- ^ 4 Players
-data P5 = P5a | P5b | P5c | P5d | P5e deriving (PLAYERID) -- ^ 5 Players
-#undef PLAYERID
+#define ENUM Eq,Ord,Bounded,Enum,Ix,Show
+data P1 = P1a                         deriving (ENUM) -- ^ 1 Player
+data P2 = P2a | P2b                   deriving (ENUM) -- ^ 2 Players
+data P3 = P3a | P3b | P3c             deriving (ENUM) -- ^ 3 Players
+data P4 = P4a | P4b | P4c | P4d       deriving (ENUM) -- ^ 4 Players
+data P5 = P5a | P5b | P5c | P5d | P5e deriving (ENUM) -- ^ 5 Players
+#undef ENUM
+
+instance PlayerId P1 where numPlayers = 1; drawValue = 1/1
+instance PlayerId P2 where numPlayers = 2; drawValue = 1/2
+instance PlayerId P3 where numPlayers = 3; drawValue = 1/3
+instance PlayerId P4 where numPlayers = 4; drawValue = 1/4
+instance PlayerId P5 where numPlayers = 5; drawValue = 1/5
 
 -- | Game result.
 data Result p = Draw | Win p deriving (Eq,Ord,Show)
--- src/mct/MCT.hs
+++ src/mct/MCT.hs
@@ -1,139 +1,87 @@
-{-# LANGUAGE AllowAmbiguousTypes  #-}
 {-# LANGUAGE TemplateHaskell      #-}
-{-# LANGUAGE UndecidableInstances #-}
 
 module MCT where
 
 import Control.Lens
-import Data.List.NonEmpty (NonEmpty)
+import Control.Monad.Random.Class ( MonadRandom, getRandomR )
 import Data.List.NonEmpty qualified as NE
 import Data.Tree
-import Data.Tree.Lens
 
-import Control.Monad.Random  ( evalRand, runRand, getRandomR )
-import System.Random         ( StdGen, newStdGen, split )
-import System.Random.Shuffle ( shuffleM )
-
-import Total
 import Game
 
 data MCT g = MCT
     { _mctGame   :: g
+    , _mctScore  :: Double
     , _mctVisits :: Double
-    , _mctDraws  :: Double
-    , _mctWins   :: UArray (Player g) Double
     }
 makeLenses ''MCT
 
-initMCT g = MCT g 0 0 (totalArray $ repeat 0)
+initMCT g = MCT g 0 0
 
 mkTree g = initMCT <$> tree g where
     tree  = ap Node (map tree . nextGames)
 
-param = sqrt 2 :: Double
-
-eval numPlayers player logParent MCT{..} =
-    exploit + param * explore
+addResult player r (m :: MCT g) = m
+    & over mctScore (+score)
+    . over mctVisits (+1)
   where
-    score = _mctWins^.totalAt player + _mctDraws / numPlayers
-    exploit = score / _mctVisits
+    score = case r of
+        Draw  -> drawValue @(Player g)
+        Win p -> bool 0 1 $ p == player
+
+data Child = Select Double | Expand deriving (Eq,Ord,Show)
+
+eval logParent MCT{..}
+    | _mctVisits == 0 = Expand -- Expand > Select _
+    | otherwise       = Select $ exploit + param * explore
+  where
+    exploit = _mctScore / _mctVisits
     explore = sqrt (logParent / _mctVisits)
-
-addResult r = over mctVisits (+1) . case r of
-    Draw  -> over mctDraws (+1)
-    Win p -> over (mctWins . totalAt p) (+1)
-
-pickOne l = getRandomR (0, length l - 1) <&> (l!!)
+    param = sqrt 2
 
 rollout g = gameStatus g & maybe next pure where
     next = pickOne (nextGames g) >>= rollout
+    pickOne l = getRandomR (0, length l - 1) <&> (l!!)
 
-search node@(Node m tt)
-    | null tt = terminal
-    | null uu = selection
-    | null vv = expansion
+search player node@(Node m@MCT{..} tt) = case gameStatus _mctGame of
+    -- Terminal node
+    Just r -> pure $ update m tt r
+    -- Unexplored node
+    Nothing | _mctVisits == 0 -> rollout _mctGame <&> update m tt
+    -- Select "best" child
+    Nothing -> do
+        (r,child') <- search (gamePlayer _mctGame) child
+        pure $ update m (aa <> [child'] <> bb) r
+      where
+        vv = eval (log _mctVisits) . rootLabel <$> tt
+        i = fst $ maximumBy (comparing snd) $ zip [0..] vv
+        (aa,child:bb) = splitAt i tt
   where
-    isVisited (Node m _) = m^.mctVisits > 0
-    (vv,uu@(~(u:uu'))) = span isVisited tt
-    terminal =
-        let r = m^.mctGame.to gameStatus._Just
-        in (r, addResult r m)
-    selection =
-        let vv = view (root.to eval) <$> tt
-            Just i = findIndex (==maximum vv) vv
-        in 
+    update m tt r = (r, Node (m & addResult player r) tt)
 
-        
+mctSearch player = fmap snd . search player
 
--- search node =
+bestMove (Node _ tt) =
+    maximumBy (comparing _mctVisits) (rootLabel <$> tt)
+    & NE.head . gameMoves . _mctGame
 
+pp MCT{..} =
+    [ show $ NE.head $ gameMoves _mctGame
+    , show $ round _mctVisits
+    , show $ round $ 100 * _mctScore / _mctVisits
+    ]
 
--- search node@(Node (Right ggen) tt) =
---     rollout ggen <&> \m -> set root (Left m) node
+tabulate stuff =
+    [ intercalate " " $ zipWith align ww ss
+    | ss <- stuff ]
+  where
+    ww = maximum . map length <$> transpose stuff
+    align w s = replicate (w - length s) ' ' <> s
 
--- search node@(Node (Left m) tt)
---     | null tt   = terminal
---     | null rr   = selection
---     | otherwise = expansion ll t rr'
---   where
---     terminal =
---         let plays = m ^. mctVisits in
---         let reward = totalArray (m ^. mctReward . to elems . to (map (/plays))) in
---         (reward, node & over (root._Left) (over mctVisits (+1) . over mctReward (addReward reward)))
---     isLeftTree (Node a _) = isLeft a
---     (ll,rr@(~(t:rr'))) = span isLeftTree tt
---     param     = sqrt 2
---     player    = m ^. mctGame . to gamePlayer
---     logParent = m ^. mctVisits . to log
---     eval (Node (Left child) _) = exploit + param * explore where
---         exploit = child ^. mctReward . totalAt player / child ^. mctVisits
---         explore = sqrt (logParent / child ^. mctVisits)
---     selection =
---         -- traceShow ("select best move for", player)
---         let vv = eval <$> tt in
---         let vmax = maximum vv in
---         let Just i = findIndex (==vmax) vv in
---         let (aa,u:bb) = splitAt i tt in
---         expansion aa u bb
---     expansion aa u bb =
---         let (reward,v) = search u in
---         let m' = m & over mctVisits (+1) . over mctReward (addReward reward) in
---         (reward, Node (Left m') (aa <> [v] <> bb))
---     addReward aa bb =
---         totalArray $ zipWith (+) (elems aa) (elems bb)
-
--- treeItem (Node x _) = either Left (Right . fst) x
-
--- pp (Right (g,_)) = showGame g
--- pp (Left MCT{..}) =
---     show (_mctVisits, pct _mctVisits <$> elems _mctReward)
---     <> " " <> showGame _mctGame
-
--- showGame g = show (gamePlayer g) <> " " <> show (NE.toList $ gameMoves g)
-
--- pct d n = round $ 100 * n / d
-
--- mctSearchTree n g = do
---     gen <- newStdGen
---     let t = fmap Right . randomize gen $ gameTree g
---     pure $ iterate go t !! n
---   where
---     go t = snd $ search t
-
--- mctSearch n g = mctSearchTree n g <&> bestMove
-
--- bestMove (Node (Right _) _) = error "unsearched node"
--- bestMove (Node _ tt)
---     | null cands = error "no children"
---     | otherwise = lastMove $ maximumBy (comparing _mctVisits) cands
---   where
---     cands = lefts (view root <$> tt)
-
--- lastMove :: Game g => MCT g -> Move g
--- lastMove = NE.head . gameMoves . _mctGame
-
--- mctTest g n p =
---     mctSearchTree n g >>= out
---   where
---     out = traverse_ putStrLn . trim . lines . drawTree . fmap pp . prune p
---     trim = filter (any isAlpha)
+mctTest p g n = do
+    t <- go n (mkTree g)
+    traverse_ putStrLn $ tabulate $ pp . rootLabel <$> subForest t
+    print $ bestMove t
+  where
+    go 0 t = pure t
+    go n t = mctSearch p t >>= go (n-1)
