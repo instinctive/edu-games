c2f4b5a10e9a396c76262b8ff32943e7419c6383
instinctive <177694+instinctive@users.noreply.github.com>
2026-01-21 12:14:23 -0800

    Progress on squares, pieces, and bitboard

--- src/chess/Chess.hs
+++ src/chess/Chess.hs
@@ -6,6 +6,7 @@
 module Chess where
 
 import Control.Lens hiding ( universe )
+import Data.Array.Base ( unsafeAt, unsafeReplace )
 import Data.Array.Unboxed
 import Data.Bits.Lens ( bitAt )
 import Data.Text ( Text )
@@ -19,27 +20,70 @@ import Game
 
 #define ENUM Eq,Ord,Bounded,Enum,Ix,Show
 data Color = White | Black deriving (ENUM)
-data Piece = Pawn | Knight | Bishop | Rook | Queen | King deriving (ENUM)
+data PType = Pawn | Knight | Bishop | Rook | Queen | King deriving (ENUM)
 #undef ENUM
 
 class (Eq a, Ord a, Bounded a, Enum a, Ix a, Show a) => ENUM a
 instance ENUM Color
-instance ENUM Piece
+instance ENUM PType
+
+data Piece = Piece { _pColor :: !Color, _pType :: !PType }
+makeLenses ''Piece
+
+instance Show Piece where
+    show Piece{..} = [color _pColor, ptype _pType] & adjust _pColor where
+        adjust White = map toUpper
+        adjust Black = id
+        color White  = 'w'
+        color Black  = 'b'
+        ptype Pawn   = 'p'
+        ptype Knight = 'n'
+        ptype Bishop = 'b'
+        ptype Rook   = 'r'
+        ptype Queen  = 'q'
+        ptype King   = 'k'
 
 -- }}}
 
 -- Squares {{{
 -- ------------------------------------------------------------
 
-newtype Sq = Sq Int
 newtype File = File Int
 newtype Rank = Rank Int
-
 instance Show File where show (File i) = [ chr $ ord 'a' + i ]
 instance Show Rank where show (Rank i) = [ intToDigit $ i + 1 ]
+
+-- File and Rank constants {{{
+fileA = File 0
+fileB = File 1
+fileC = File 2
+fileD = File 3
+fileE = File 4
+fileF = File 5
+fileG = File 6
+fileH = File 7
+
+rank1 = Rank 0
+rank2 = Rank 1
+rank3 = Rank 2
+rank4 = Rank 3
+rank5 = Rank 4
+rank6 = Rank 5
+rank7 = Rank 6
+rank8= Rank 7
+-- }}}
+
+newtype Sq = Sq Int
+
+sqFileRank (Sq q) = bimap File Rank (unsafeShiftR q 3, q .&. 7)
+mkSq (File file) (Rank rank) = Sq $ unsafeShiftL file 3 .|. rank
+allFiles = File <$> [0..7]
+rankSqs rank = flip mkSq rank <$> allFiles
+
 instance Show Sq where
-    show (Sq q) = show file <> show rank where
-        (file,rank) = bimap File Rank $ quotRem q 8
+    show sq = show file <> show rank where
+        (file,rank) = sqFileRank sq
+
 instance Read Sq where
     readsPrec _ (c:d:more) = maybeToList $ (,more) <$> readSq [c,d]
     readsPrec _ _ = []
@@ -51,47 +95,68 @@ readSq [c,d] = mk <$> file <*> rank where
 
 instance IsString Sq where fromString = fromJust . readSq
 
+toBlack, toWhite, toKSide, toQSide :: (File,Rank) -> Maybe (File,Rank)
+toBlack (file, Rank i) = i < 7 & bool Nothing (Just (file, Rank (i+1)))
+toWhite (file, Rank i) = i > 0 & bool Nothing (Just (file, Rank (i-1)))
+toKSide (File i, rank) = i < 7 & bool Nothing (Just (File (i+1), rank))
+toQSide (File i, rank) = i > 0 & bool Nothing (Just (File (i-1), rank))
+
+toColor Black = toBlack
+toColor White = toWhite
+
+-- }}}
+
+-- Total {{{
+-- ------------------------------------------------------------
+
+universe :: forall a. (Bounded a, Enum a) => [a]
+universe = [minBound..maxBound]
+
+newtype Total a i e = Total (a i e) deriving Show
+
+totalArray :: forall a i e. (IArray a e, ENUM i) => [e] -> Total a i e
+totalArray = Total . listArray (minBound,maxBound)
+
+totalAt :: forall a i e. (IArray a e, ENUM i) => i -> Lens' (Total a i e) e
+totalAt i f (Total ary) = let x = fromEnum i in
+    f (unsafeAt ary x) <&> \e -> Total $ unsafeReplace ary [(x,e)]
+
 -- }}}
 
 -- BitBoard {{{
 -- ------------------------------------------------------------
 
 data BitBoard = BitBoard
-    { _bbColor :: !(UArray Color Word64)
-    , _bbPiece :: !(UArray Piece Word64)
+    { _bbColor :: !(Total UArray Color Word64)
+    , _bbPType :: !(Total UArray PType Word64)
     } deriving Show
 makeLenses ''BitBoard
 
-universe :: forall a. (Bounded a, Enum a) => [a]
-universe = [minBound..maxBound]
-
-totalArray :: forall i e. (ENUM i, IArray UArray e) => [e] -> UArray i e
-totalArray = listArray (minBound,maxBound)
-
-aryAt :: Ix i => i -> Lens' (UArray i Word64) Word64
-aryAt i f ary = f (ary!i) <&> \e -> ary // [(i,e)]
-
-bbAt :: Sq -> Lens' BitBoard (Maybe (Color,Piece))
+bbAt :: Sq -> Lens' BitBoard (Maybe Piece)
 bbAt (Sq q) f bb@BitBoard{..} =
     f m <&> \m' -> case (m,m') of
-        (Nothing,Nothing) -> bb
-        (Just (c,p),Nothing) ->
-            bb & set (bbColor.aryAt c.bitAt q) False
-               . set (bbPiece.aryAt p.bitAt q) False
-        (Nothing,Just (c',p')) ->
-            bb & set (bbColor.aryAt c'.bitAt q) True
-               . set (bbPiece.aryAt p'.bitAt q) True
-        (Just (c,p),Just (c',p')) ->
-            bb & set (bbColor.aryAt c.bitAt q) False
-               . set (bbPiece.aryAt p.bitAt q) False
-               . set (bbColor.aryAt c'.bitAt q) True
-               . set (bbPiece.aryAt p'.bitAt q) True
+        (Nothing,   Nothing)   -> bb
+        (Just prev, Nothing)   -> bb & update prev False
+        (Nothing,   Just next) -> bb & update next True
+        (Just prev, Just next) -> bb & update prev False . update next True
   where
-    m = (,) <$> c <*> p
-    c = listToMaybe [ i | i <- universe, testBit (_bbColor!i) q ]
-    p = listToMaybe [ i | i <- universe, testBit (_bbPiece!i) q ]
+    m = Piece <$> c <*> p
+    c = listToMaybe [ i | i <- universe, testBit (_bbColor^.totalAt i) q ]
+    p = listToMaybe [ i | i <- universe, testBit (_bbPType^.totalAt i) q ]
+    update (Piece c p) b
+      = set (bbColor.totalAt c.bitAt q) b
+      . set (bbPType.totalAt p.bitAt q) b
 
-initBitBoard = BitBoard (totalArray $ repeat 0) (totalArray $ repeat 0)
+emptyBitBoard = BitBoard (totalArray $ repeat 0) (totalArray $ repeat 0)
+
+startBitBoard = foldl' f emptyBitBoard $
+    zip (rankSqs rank8) (Piece Black <$> ptypes) <>
+    zip (rankSqs rank1) (Piece White <$> ptypes) <>
+    zip (rankSqs rank7) (replicate 8 (Piece Black Pawn)) <>
+    zip (rankSqs rank2) (replicate 8 (Piece White Pawn))
+  where
+    ptypes = [Rook,Knight,Bishop,Queen,King,Bishop,Knight,Rook]
+    f !bb (sq,piece) = bb & bbAt sq .~ Just piece
 
 -- }}}
 
